20346d8b3b7321907b00a22342f2df0919e44180
Adam Schmalhofer <code@adaschma.name>
2021-12-05 12:27:23 +0100

    Call Grocy Api

--- grocy-importer.py
+++ grocy-importer.py
@@ -1,10 +1,39 @@
 #!/usr/bin/python3
 
-from sys import argv
+import re
+from sys import argv, exit
 from email.parser import Parser
-from typing import List, Union
+from typing import List, Union, Iterable
 from dataclasses import dataclass
+from itertools import groupby
+from configparser import ConfigParser
+from os.path import dirname, abspath, join
+
 from bs4 import BeautifulSoup
+import requests
+
+
+class GrocyApi:
+
+    def __init__(self, api_key, base_url):
+        self.HEADERS = {'GROCY-API-KEY': api_key}
+        self.BASE_URL = base_url
+
+    def get_all_products(self):
+        response = requests.get(self.BASE_URL + '/objects/products',
+                                headers=self.HEADERS)
+        return {p['name']: p['id'] for p in response.json()}
+
+    def purchase(self, productId: int, amount: int, price: str):
+        CALL = f'/stock/products/{productId}/add'
+        response = requests.post(self.BASE_URL + CALL,
+                                 headers=self.HEADERS,
+                                 json={'amount': amount, 'price': price})
+        assert(response.status_code/100 == 2)
+
+
+def cleanup_product_name(orig: str) -> str:
+    return re.sub(r'\s+', ' ', orig)
 
 
 @dataclass
@@ -13,42 +42,88 @@ class Purchase:
     price: str
     name: str
 
-    def __init__(self, args):
-        if len(args) == 2:
-            self.name = args[0]
-            self.price = from_netto_price(args[1])
-            self.amount = 1
-        else:
-            self.name = args[1]
-            self.price = from_netto_price(args[2])
-            self.amount = float(args[0].split()[0])
+
+def parse_purchase(args):
+    return (Purchase(1,
+                     from_netto_price(args[1]),
+                     cleanup_product_name(args[0]))
+            if len(args) == 2
+            else Purchase(float(args[0].split()[0]),
+                          from_netto_price(args[2]),
+                          cleanup_product_name(args[1])))
 
 
 def from_netto_price(netto_price: str) -> str:
     return netto_price.split()[0].replace(',', '.')
 
 
-with open(argv[1], 'r', encoding='utf-8') as f:
-    email = Parser().parse(f)
-html = list(part
-            for part in email.walk()
-            if part.get_content_type() == 'text/html'
-            )[0].get_payload(decode=True)
-soup = BeautifulSoup(html, 'html5lib')
-purchase = list(column.get_text()
-                for row
-                in soup.select(' '.join(8*["tbody"] + ["tr"]))
-                if not list(row.select('td'))[0].get_text().endswith(':')
-                and not any(keyword in row.get_text()
-                            for keyword in ['Filiale', 'Rabatt',
-                                            'DeutschlandCard'])
-                for column in row.select('td')
-                if column.get_text() != ''
-                )
-items: List[List[str]] = []
-for p in purchase:
-    if p.isspace():
-        items.append([])
-    else:
-        items[-1].append(p)
-print([Purchase(item) for item in items if len(item) > 1])
+def simplify(items: Iterable[Purchase]) -> List[Purchase]:
+    '''
+    >>> simplify([parse_purchase(['Milch', '1,00']),
+    ... parse_purchase(['Mehl', '2,00'])])
+    [Purchase(amount=1, price='2.00', name='Mehl'), Purchase(amount=1, price='1.00', name='Milch')]
+
+    >>> simplify([parse_purchase(['Milch', '1,00']),
+    ... parse_purchase(['Milch', '1,00'])])
+    [Purchase(amount=2, price='1.00', name='Milch')]
+    >>> simplify([parse_purchase(['Milch', '1,00']),
+    ... parse_purchase(['Mehl', '2,00']),
+    ... parse_purchase(['Milch', '1,00'])])
+    [Purchase(amount=1, price='2.00', name='Mehl'), Purchase(amount=2, price='1.00', name='Milch')]
+
+    '''
+    return [Purchase(sum(p.amount for p in g),
+                     price,
+                     name)
+            for (name, price), g
+            in groupby(sorted(items, key=lambda p: p.name),
+                       lambda p: (p.name, p.price))]
+
+
+def netto_purchase():
+    with open(argv[1], 'r', encoding='utf-8') as f:
+        email = Parser().parse(f)
+    html = list(part
+                for part in email.walk()
+                if part.get_content_type() == 'text/html'
+                )[0].get_payload(decode=True)
+    soup = BeautifulSoup(html, 'html5lib')
+    purchase = list(column.get_text()
+                    for row
+                    in soup.select(' '.join(8*["tbody"] + ["tr"]))
+                    if not list(row.select('td'))[0].get_text().endswith(':')
+                    and not any(keyword in row.get_text()
+                                for keyword in ['Filiale', 'Rabatt',
+                                                'DeutschlandCard'])
+                    for column in row.select('td')
+                    if column.get_text() != ''
+                    )
+    items: List[List[str]] = []
+    for p in purchase:
+        if p.isspace():
+            items.append([])
+        else:
+            items[-1].append(p)
+    return simplify(parse_purchase(item) for item in items if len(item) > 1)
+
+
+def main():
+    config = ConfigParser()
+    config.read(join(dirname(abspath(argv[0])), 'config.ini'))
+    groceries = netto_purchase()
+    grocy = GrocyApi(**config['grocy'])
+    known_products = grocy.get_all_products()
+    unknown_items = [str(item)
+                     for item in groceries
+                     if item.name not in known_products]
+    if any(unknown_items):
+        print('Unknown products. Please add to grocy:')
+        print('\n'.join(unknown_items))
+        exit(1)
+    for item in groceries:
+        grocy.purchase(known_products[item.name], item.amount, item.price)
+        print(f'Added {item}')
+
+
+if __name__ == '__main__':
+    main()
