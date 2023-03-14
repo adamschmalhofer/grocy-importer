434a6d1aa1ce2d413e0d251f0df2cb88cbfa781a
Adam Schmalhofer <code@adaschma.name>
2023-03-14 22:53:58 +0100

    Port from Makefile to tox

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='grocy_importer',
+      version='0.1',
+      description='Help importing into and from Grocy',
+      url='https://gitlab.com/adaschma/grocy-importer',
+      author='Adam Schmalhofer',
+      author_email='code@adaschma.name',
+      license='MIT',
+      packages=[],
+      zip_safe=False,
+      scripts=['grocy_importer.py'],
+      install_requires=[
+            'bs4',
+            'requests',
+            'marshmallow',
+            'appdirs',
+            'argcomplete',
+          ])
